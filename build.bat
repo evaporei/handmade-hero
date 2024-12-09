7a492b9cd0fc5c7ab335bbe53ee3b4410c8c666e
Eva Pace <eba.pachi@gmail.com>
2024-12-09 12:49:37 -0300

    change argument order in build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist build mkdir build
 pushd build
-clang ..\src\main.c -lkernel32 -luser32 -lgdi32 -o main.exe
+clang ..\src\main.c -o main.exe -lkernel32 -luser32 -lgdi32
 popd
